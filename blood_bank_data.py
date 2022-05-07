ee1478eaee526c9f83c356b63b2d4baeb361a608
Harsh Mishra <hmisraji07@gmail.com>
2022-05-07 16:26:26 +0530

    Added access methods for parsed blood_bank_data

---
+++ blood_bank_data.py
@@ -0,0 +1,33 @@
+import dbdriver
+import asyncio
+async def get_blood_banks(blood_type):
+    data  = await dbdriver.getRecords('blood_samples', 'blood_type_available' , f'%{blood_type}%', modifier='LIKE' )
+    return_object = []
+    if data:
+        #if not empty_set
+        for sample in data:
+            return_object.append(
+                {
+                    "ORG Name" : sample[0],
+                    "Blood Group Available" : sample[1],
+                    "Location" : sample[2] 
+                }
+            )
+        return return_object
+    else:
+        return ()
+
+
+# async def test():
+#     data = await get_blood_banks('A+')
+#     if data:
+#         for sample in data:
+#             print(sample)
+#     else:
+#         print("No compatible samples")
+
+
+    
+# if __name__ == '__main__':
+#     asyncio.run(test())
+
